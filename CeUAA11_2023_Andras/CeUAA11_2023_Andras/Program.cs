181b341c1b3c185104cb646c8809ce3685a1d443
Andras Corda <115708078+R3tr0-andras@users.noreply.github.com>
2023-06-01 16:07:41 +0200

    Add files via upload

---
+++ CeUAA11_2023_Andras/CeUAA11_2023_Andras/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace CeUAA11_2023_Andras
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            other mesOutils = new other();
+
+            int a = 0;
+            bool recom = false;
+
+            //boucle pour recommencer
+            while (recom == false)
+            {
+                Console.Clear();
+                Console.WriteLine("fait un choix\n 1. cryptage Vigenere\n 2. cryptage Affine");
+                string choix = Console.ReadLine();
+                
+                switch (a)
+                {
+                    //Vigenere
+                    case 0:
+                        Console.Clear();
+                        //demander le message
+                        Console.WriteLine("message a crypter");
+                        string phClaire = Console.ReadLine();
+
+                        Console.Clear();
+                        //demander la cle
+                        Console.WriteLine("cle de cryptage");
+                        string phCle = Console.ReadLine();
+
+                        Console.Clear();
+                        //utiliser MP
+                        string resVig = other.matVigenere(phClaire, phCle);
+                        Console.WriteLine("voici : \n" + resVig);
+
+                        //demander recommencer
+                        Console.WriteLine("\nvoulez vous recommencer ?");
+                        string reponse1 = Console.ReadLine();
+                        recom = (reponse1 == "non");
+
+                        break;
+                    //Affine
+                    case 1:
+                        Console.Clear();
+                        //demander a
+                        Console.WriteLine("donner a");
+                        int aa = int.Parse(Console.ReadLine());
+
+                        Console.Clear();
+                        //demander b
+                        Console.WriteLine("donner b");
+                        int b = int.Parse(Console.ReadLine());
+
+                        Console.Clear();
+                        //demander le message
+                        Console.WriteLine("message a crypter");
+                        phClaire = Console.ReadLine();
+
+                        Console.Clear();
+                        //demander la cle
+                        Console.WriteLine("cle de cryptage");
+                        phCle = Console.ReadLine();
+
+                        Console.Clear();
+                        //utiliser MP
+                        string resAff = other.CryptAffine(phClaire, phCle, a, b);
+                        Console.WriteLine("voici : \n" + resAff);
+
+                        //demander recommencer
+                        Console.WriteLine("\nvoulez vous recommencer ?");
+                        string reponse2 = Console.ReadLine();
+                        recom = (reponse2 == "non");
+
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("recommencer");
+                        break;
+
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("hello world");
+        }
+    }
+}
---
+++ CeUAA11_2023_Andras/CeUAA11_2023_Andras/other.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CeUAA11_2023_Andras
+{
+    public struct other
+    {
+        //Cryptage en Vigenere
+        public static string matVigenere(string phClaire, string phCle)
+        {
+            string[,] matVigenere = new string[4, phClaire.Length];
+            int j = 0;
+            int codeAscii;
+
+            for (int i = 0; i < phClaire.Length - 1; i++)
+            {
+                matVigenere[0, i] = phClaire[i].ToString();
+
+                if (j == phCle.Length)
+                {
+                    j = 0;
+                }
+                matVigenere[1, i] = phCle[j].ToString();
+                matVigenere[2, i] = ((int)phCle[j] - 65).ToString();
+
+                if ((int)phClaire[i] + int.Parse(matVigenere[2, i]) <= 90)
+                {
+                    codeAscii = (int)char.Parse(matVigenere[2, i]);
+                }
+                else
+                {
+                    codeAscii = (int)phClaire[i] + int.Parse(matVigenere[2, i]) - 26;
+                }
+                matVigenere[3, i] = Convert.ToChar(codeAscii).ToString();
+                j = j + 1;
+            }
+
+            //sortir le résultat
+            string resultat = "";
+            for (int k = 0; k < phClaire.Length; k++)
+            {
+                resultat += matVigenere[3, k];
+            }
+            return resultat;
+        }
+
+        //Cryptage en Affine
+        public static string CryptAffine(string phClaire, string phCle, int aa, int b)
+        {
+            string[,] matAffine = new string[4, phClaire.Length];
+
+            for (int i = 0; i < phClaire.Length - 1; i++)
+            {
+                matAffine[0, i] = phClaire[i].ToString();
+                int x = ((int)phClaire[i] - 65);
+                matAffine[1, i] = x.ToString();
+                int y = (aa * x + b) % 26;
+                matAffine[2, i] = y.ToString();
+                matAffine[3, i] = ((char)(y + 65)).ToString();
+            }
+
+            //sortir le résultat
+            string result = "";
+            for (int j = 0; j < phClaire.Length; j++)
+            {
+                result += matAffine[3, j];
+            }
+            return result;
+        }
+    }
+
+}
